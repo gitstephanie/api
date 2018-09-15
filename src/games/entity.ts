a37a4732d75130bc1d06f5ac81b799c584c3ca62
gitstephanie <s.dejong@mail.com>
2018-09-15 11:36:27 +0200

    Set-up API and structure

---
+++ src/games/entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString, IsJSON } from 'class-validator'
+
+@Entity()
+export default class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @IsString()
+    @Column('text', {nullable:false})
+    name: string
+    
+  @IsString()
+    @Column('text', {nullable:false})
+    color: string
+  
+  @IsJSON()
+    @Column('text', {nullable:false})
+    board: JSON
+
+}
